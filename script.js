3d0f9e3faa38fa406d8858e3a4ccbd88649d3c80
donabinu921 <elizabethdona921@gmail.com>
2023-05-21 14:27:24 +0530

    findMatches() written

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const cities=[];
 fetch(endpoint)
 .then(response => response.json())
 .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.state.match(regex)
+    });
+  }
